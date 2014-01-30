516571a3f0548ebf8ec55e47851bfee97f6bdd20
Brandon Taylor <alsoicode@gmail.com>
2014-01-30 14:38:20 -0500

    Update index.js
    Added lowercase conversion of environment variable. On OS X, this variable is returned lowercase. On Linux, it will be returned uppercase, which will cause other code which attempts to load test.json, or any other environment-specifically-named file to fail on Linux.

--- chapter-2/lib/configuration/index.js
+++ chapter-2/lib/configuration/index.js
@@ -2,7 +2,7 @@ var nconf = require('nconf');
 
 function Config(){
   nconf.argv().env('_');
-  var environment = nconf.get('NODE:ENV') || 'development';
+  var environment = nconf.get('NODE:ENV').toLowerCase() || 'development';
   nconf.file(environment, 'config/' + environment + '.json');
   nconf.file('default', 'config/default.json');
 }
